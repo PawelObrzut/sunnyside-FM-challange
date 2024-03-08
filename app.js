b9203b7c43364fcc2bedd023b9dc2480a129bf74
PawelObrzut <pawel.r.obrzut@gmail.com>
2024-03-08 18:53:36 +0100

    adds event listeners to header section

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const body = document.querySelector('body');
-
 const menuButton = document.querySelector(".navbar-menu--icon");
-
 const mobileMenu = document.querySelector(".navbar--list");
 
 menuButton.addEventListener("click", () => {
@@ -12,4 +10,11 @@ body.addEventListener("click", (e) => {
   if (!mobileMenu.contains(e.target) && e.target !== menuButton) {
     mobileMenu.classList.add("hidden");
   }
+});
+
+[...mobileMenu.children].forEach(li => {
+  li.addEventListener('click', () => {
+    document.querySelector('.active') ? document.querySelector('.active').classList.remove('active') : '';
+    li.classList.add('active')
+  })
 });
