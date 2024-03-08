05b7cad646deee6c0d7d78852d010be6956c8c01
PawelObrzut <pawel.r.obrzut@gmail.com>
2024-03-08 10:56:14 +0100

    completes the header layout with basic js functionality

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Lets begin")
+const body = document.querySelector('body');
+
+const menuButton = document.querySelector(".navbar-menu--icon");
+
+const mobileMenu = document.querySelector(".navbar--list");
+
+menuButton.addEventListener("click", () => {
+  mobileMenu.classList.toggle("toggle-display");
+});
+
+body.addEventListener("click", (e) => {
+  if (!mobileMenu.contains(e.target) && e.target !== menuButton) {
+    mobileMenu.classList.add("toggle-display");
+  }
+});
